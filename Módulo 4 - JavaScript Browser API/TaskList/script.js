35d243361eb73eed0fba78288da4379349e52967
Bentroen <bemcdc@gmail.com>
2021-11-05 05:03:35 -0300

    TaskList: Add icon buttons and remove placeholder text on task add

--- Módulo 4 - JavaScript Browser API/TaskList/script.js
+++ Módulo 4 - JavaScript Browser API/TaskList/script.js
@@ -22,10 +22,22 @@ function adicionarTarefa(nome, data) {
     tarefasPendentes.unshift(tarefa);
 
     let tarefaEl = tarefasPendentesEl.insertRow(0);
+    let concluidoEl = tarefaEl.insertCell();
+    concluidoEl.innerHTML = ' \
+        <span class="material-icons">check_box_outline_blank</span> \
+    '
     let nomeEl = tarefaEl.insertCell();
     nomeEl.appendChild(document.createTextNode(nome));
     let dataEl = tarefaEl.insertCell();
     dataEl.appendChild(document.createTextNode(data));
+    let acoesEl = tarefaEl.insertCell();
+    acoesEl.innerHTML = ' \
+        <button class="btn btn-delete-task"><span class="material-icons">delete</span></button> \
+        <button class="btn btn-shift-task-up"><span class="material-icons">arrow_upward</span></button> \
+        <button class="btn btn-shift-task-down"><span class="material-icons">arrow_downward</span></button> \
+    '
+
+    nenhumaTarefaPendenteEl.classList.add("hidden")
 }
 
 function criarTarefa() {
