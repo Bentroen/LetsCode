9e051251081c7e09f613e4d30f1a4af46578594f
Bentroen <bemcdc@gmail.com>
2021-11-24 21:07:51 -0300

    SpotifyApp: Create components and services

--- Módulo 5 - Angular/SpotifyApp/src/app/app.module.ts
+++ Módulo 5 - Angular/SpotifyApp/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PlaylistsComponent } from './playlists/playlists.component';
+import { PlaylistItemComponent } from './playlists/playlist-item/playlist-item.component';
+import { PlaylistItemEditComponent } from './playlists/playlist-item-edit/playlist-item-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PlaylistsComponent,
+    PlaylistItemComponent,
+    PlaylistItemEditComponent
   ],
   imports: [
     BrowserModule,
