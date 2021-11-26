292be61c251f029315d9d9f85d5a38896668474e
Bentroen <bemcdc@gmail.com>
2021-11-26 02:18:21 -0300

    Kanban: Create components

--- Módulo 5 - Angular/Kanban/FRONT/src/app/app.module.ts
+++ Módulo 5 - Angular/Kanban/FRONT/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { NewCardComponent } from './board/new-card/new-card.component';
+import { CardListComponent } from './board/card-list/card-list.component';
+import { CardComponent } from './board/card-list/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    NewCardComponent,
+    CardListComponent,
+    CardComponent,
   ],
   imports: [
     BrowserModule
