7798aa694623cba1a8d1f81f00b3926aec12b587
Bentroen <bemcdc@gmail.com>
2021-11-27 15:01:31 -0300

    Kanban: Create card model and add test cards

--- Módulo 5 - Angular/Kanban/FRONT/src/app/board/card-list/card-list.component.ts
+++ Módulo 5 - Angular/Kanban/FRONT/src/app/board/card-list/card-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Card } from 'src/app/models/card.model';
 
 @Component({
   selector: 'app-card-list',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardListComponent implements OnInit {
 
+  cards: Array<Card> = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    let card = new Card("Test", "This is a test description!");
+    this.cards.push(card);
+    console.log(this.cards);
   }
 
 }
