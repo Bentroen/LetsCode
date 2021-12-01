39bd031d6a8007fe5b121e95c121fed4c8104476
Bentroen <bemcdc@gmail.com>
2021-11-30 21:23:23 -0300

    Kanban: Add basic app routing

--- Módulo 5 - Angular/Kanban/FRONT/src/app/app.module.ts
+++ Módulo 5 - Angular/Kanban/FRONT/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
@@ -7,6 +8,7 @@ import { BoardComponent } from './board/board.component';
 import { NewCardComponent } from './board/new-card/new-card.component';
 import { CardListComponent } from './board/card-list/card-list.component';
 import { CardComponent } from './board/card-list/card/card.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -20,6 +22,8 @@ import { CardComponent } from './board/card-list/card/card.component';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
