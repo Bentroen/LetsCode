be7d82c30bcd523d5d66360ee0b5996b1c9e34d0
Bentroen <bemcdc@gmail.com>
2021-11-30 21:06:55 -0300

    Kanban: Add card creation functionality

--- Módulo 5 - Angular/Kanban/FRONT/src/app/board/new-card/new-card.component.ts
+++ Módulo 5 - Angular/Kanban/FRONT/src/app/board/new-card/new-card.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Card } from 'src/app/models/card.model';
+import { BoardService } from 'src/app/services/board.service';
 
 @Component({
   selector: 'app-new-card',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewCardComponent implements OnInit {
 
-  constructor() { }
+  cardForm = new FormGroup({
+    name: new FormControl(''),
+    description: new FormControl(''),
+  });
+
+  constructor(private boardService: BoardService) { }
 
   ngOnInit(): void {
+  }
+
+  addCard() {
+    let card: Card = {
+      "name": this.cardForm.value.name,
+      "description": this.cardForm.value.description
+    }
+    this.boardService.moveCard(card, 0);
+    this.cardForm.reset();
   }
 
 }
