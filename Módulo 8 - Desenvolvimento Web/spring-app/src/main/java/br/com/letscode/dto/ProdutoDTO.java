2612b46c2463c55622110b45a1cdaff69cae3cbf
Bentroen <bemcdc@gmail.com>
2022-02-23 20:26:05 -0300

    SpringApp: Add more comments

--- Módulo 8 - Desenvolvimento Web/spring-app/src/main/java/br/com/letscode/dto/ProdutoDTO.java
+++ Módulo 8 - Desenvolvimento Web/spring-app/src/main/java/br/com/letscode/dto/ProdutoDTO.java
@@ -7,6 +7,21 @@ import lombok.Getter;
 public class ProdutoDTO {
     // DTO = Data Transfer Object
 
+    // Objeto transitório para transferir informações.
+    // Objeto intermediário que contém o mínimo possível para transferir dados.
+
+    // É a parte da aplicação que vai organizar os dados que serão repassados para o browser,
+    // ou seja, que serão exibidos para o usuário. Assim, não precisamos exportar dados
+    // sensíveis, que são apenas armazenados no banco de dados e não devem ser mostrados
+    // para o usuário.
+
+    // ex.: Entidade tem usuário e senha
+    //      DTO só tem o usuário
+
+    // ex.: Idade
+    //      Você não armazena a idade no banco de dados, e sim a data de nascimento.
+    //      A partir dela é possível calcular a idade, signo etc.
+
     private String nome;
     private String descricao;
     private Double valor;
--- Módulo 8 - Desenvolvimento Web/spring-app/src/main/java/br/com/letscode/entidade/ProdutoEntity.java
+++ Módulo 8 - Desenvolvimento Web/spring-app/src/main/java/br/com/letscode/entidade/ProdutoEntity.java
@@ -10,6 +10,20 @@ public class ProdutoEntity {
     private String descricao;
     private Double valor;
 
+    // EXEMPLO NUM BANCO DE DADOS REAL:
+    // (Mapeando dados do produto para sua representação no banco de dados)
+
+    // @ID
+    // @GeneratedValue()
+    // private String id;
+    //
+    // @Column("NOME_DO_PRODUTO")
+    // private String nome;
+    //
+    // @NotNull
+    // private String descricao;
+    // private Double valor;
+
     public ProdutoEntity(ProdutoDTO produtoDTO) {
         this.nome = produtoDTO.getNome();
         this.descricao = produtoDTO.getDescricao();
