e1357c48a64cf78b3baecbefb8897c8fbf8cb641
Bentroen <bemcdc@gmail.com>
2022-04-11 19:46:19 -0300

    Distribuida: adicionar código base

---
+++ Módulo 10 - Tópicos Avançados/Distribuida/src/test/java/br/com/letscode/distribuida/DistribuidaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.letscode.distribuida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistribuidaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
