5324ae08e53450c63c6aa7a1451573702813c0ac
Bentroen <bemcdc@gmail.com>
2022-04-11 21:26:53 -0300

    Adicionar validation para anotação `NaoImpar`

--- Módulo 10 - Tópicos Avançados/Distribuida/src/test/java/br/com/letscode/distribuida/ValidarClasses.java
+++ Módulo 10 - Tópicos Avançados/Distribuida/src/test/java/br/com/letscode/distribuida/ValidarClasses.java
@@ -1,16 +1,25 @@
-package java.br.com.letscode.distribuida;
+package br.com.letscode.distribuida;
 
 import br.com.letscode.distribuida.model.Produto;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Set;
 
 public class ValidarClasses {
 
+    final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
     @Test
     public void validaPacotes() {
+
+        /*
         Produto produto = new Produto();
 
         Class<?> produtoClass = produto.getClass();
@@ -35,7 +44,12 @@ public class ValidarClasses {
             System.out.println(atributos.getName());
         }
 
-        //
+        */
+
+    }
+
+    @Test
+    public void validaAnnotationNaoImpar() {
 
         Produto produto2 = Produto.builder()
                 .nome("Camiseta")
@@ -43,7 +57,9 @@ public class ValidarClasses {
                 .valor("29,90")
                 .build();
 
+        Set<ConstraintViolation<Produto>> violations = validator.validate(produto2);
+        System.out.println(violations.isEmpty());
+        Assert.isTrue(violations.isEmpty(), "A quantidade não deve ser ímpar");
+
     }
-
-
 }
