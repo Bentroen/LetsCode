6030b51ae9983051f50c75a9fa82afcdf19574f2
Bentroen <bemcdc@gmail.com>
2022-05-16 21:25:21 -0300

    classes-api: Add base project

---
+++ Módulo 11 - Deploy, Nuvem e Microsserviços/classes-api/src/test/java/br/com/letscode/classesapi/ClassesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.letscode.classesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
